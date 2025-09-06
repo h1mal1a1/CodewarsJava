fad4fd62a0920cb025ee47c647f38f29b8b5602e
Anton <45577251+h1mal1a1@users.noreply.github.com>
2025-09-06 16:37:03 +0300

    8kyu_Grasshopper - Summation
    Summation
    Write a program that finds the summation of every number from 1 to num (both inclusive). The number will always be a positive integer greater than 0. Your function only needs to return the result, what is shown between parentheses in the example below is how you reach that result and it's not part of it, see the sample tests.
    For example (Input -> Output):
    2 -> 3 (1 + 2)
    8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)

---
+++ GrassHopper.java
@@ -0,0 +1,8 @@
+public class GrassHopper {
+    public static int summation(int n) {
+        var sum = 0;
+        for (int i = 1; i <= n; i++)
+            sum += i;
+        return sum;
+    }
+}
